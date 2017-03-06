986bb2ad244b30b6698644b33a7f5e20efe008d1
david942j <david942j@gmail.com>
2017-03-06 21:27:25 +0800

    tiny change of default.rb

--- config/defaults/default.rb
+++ config/defaults/default.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 # encoding: ascii-8bit
 require_relative '../../zocket/zocket'
+require 'pry'
 require 'pwn'      # https://github.com/peter50216/ruby-pwntools
 require 'heapinfo' # https://github.com/david942j/heapinfo
 require 'one_gadget' # https://github.com/david942j/one_gadget
 
 # @magic = one_gadget(file: './libc.so.6')
 
-HOST, PORT = '', 31337
+host, port = '', 31337
 @local = false
-(HOST = '127.0.0.1'; @local = true) if ARGV.empty?
-$z = Zocket.new HOST, PORT #, logger: HexLogger.new
+(host = '127.0.0.1'; @local = true) if ARGV.empty?
+$z = Zocket.new host, port #, logger: HexLogger.new
 def z;$z;end
 $h = heapinfo('')
-def h;$h;end
+def h;$h.reload!;end
 #================= Exploit Start ====================
 
 z.interact
