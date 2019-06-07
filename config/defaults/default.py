a5559b9ba1d4126cade85272663d808c632bc9c5
david942j <david942j@gmail.com>
2019-06-06 19:49:50 -0700

    Update default.*

--- config/defaults/default.py
+++ config/defaults/default.py
@@ -9,8 +9,9 @@ local = False
 if len(sys.argv) == 1:
   host = '127.0.0.1'
   local = True
-
 r = remote(host, port)
 
+context.arch = 'amd64'
+#  context.log_level = 'debug'
 
 r.interactive()
--- config/defaults/default.rb
+++ config/defaults/default.rb
@@ -5,23 +5,23 @@ require 'pwn'        # https://github.com/peter50216/pwntools-ruby
 require 'heapinfo'   # https://github.com/david942j/heapinfo
 require 'one_gadget' # https://github.com/david942j/one_gadget
 
-# @magic = one_gadget(file: './libc.so.6')[0]
-
 host, port = '', 31337
 @local = false
+@p = ''
 if ARGV.empty?
   host = '127.0.0.1'; @local = true
+  # $z = Tubes::Process.new(@p)
 else
   raise ArgumentError, 'host not set' if host.empty?
 end
-$z = Sock.new host, port
+$z = Sock.new(host, port)
 def z;$z;end
-@p = ''
 def h;@h ||= heapinfo(@p);end
 def elf; @elf ||= ELF.new(@p); end
+def libc; @libc ||= ELF.new('./libc.so.6'); end
 def debug!; context.log_level = :debug; end
 #================= Exploit Start ====================
-# context.arch = :amd64
+context.arch = :amd64
 # debug!
 
 z.interact
